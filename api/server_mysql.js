261bb72dc20bab991fcf3ed2568bffbce0f6622a
Venkatesh <mogilivenkatesh3@gmail.com>
2020-01-22 00:28:16 +0530

    Employee and Leaves Added

--- api/server_mysql.js
+++ api/server_mysql.js
@@ -3,8 +3,8 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
 const mysqlconnection = require('./connection');
-const {addEmployee, getEmployees, loginEmployee, editEmployee, removeEmployee} = require('./routes/employees');
-const {applyLeave, leaveHistory, pendingLeave, approveLeave, rejectLeave} = require('./routes/leaves');
+const {addEmployee, getEmployees, getEmployee, loginEmployee, editEmployee, removeEmployee} = require('./routes/employees');
+// const {applyLeave, leaveHistory, pendingLeave, approveLeave, rejectLeave} = require('./routes/leaves');
 
 const port = process.env.PORT||8080;
 
@@ -24,11 +24,11 @@ app.get(baseUrl+'/getEmployees', getEmployees);
 app.get(baseUrl+'/getEmployee/:employee_id', getEmployee);
 
 //Leave Management API's
-app.post(baseUrl+'/applyLeave', applyLeave);
-app.get(baseUrl+'/pendingLeave/:employee_id', pendingLeave);
-app.get(baseUrl+'/leaveHistory/:employee_id', leaveHistory);
-app.put(baseUrl+'/approveLeave/:leave_id', approveLeave);
-app.put(baseUrl+'/rejectLeave/:leave_id', rejectLeave);
+// app.post(baseUrl+'/applyLeave', applyLeave);
+// app.get(baseUrl+'/pendingLeave/:employee_id', pendingLeave);
+// app.get(baseUrl+'/leaveHistory/:employee_id', leaveHistory);
+// app.put(baseUrl+'/approveLeave/:leave_id', approveLeave);
+// app.put(baseUrl+'/rejectLeave/:leave_id', rejectLeave);
 
 app.listen(port,()=>{
 	console.log(`Intranet NodeJS server is running on port ${port}`);
